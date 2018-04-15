32024c818d9c850e6176eb04e6294c3a9dd4be7c
naveen <naveen@perfios.com>
2018-04-16 00:38:27 +0530

    Minor fix.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var hbs = require('hbs');
 var fs = require('fs');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || '3000';
 
 var app = express();
 
